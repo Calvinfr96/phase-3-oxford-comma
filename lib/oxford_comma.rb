7158d6b6dba85c60d3802bce40aca296f4ebc1e5
calvin <calvin.francis96@outlook.com>
2021-07-03 16:53:41 -0400

    Done with assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+test_array = ["fiddleheads","okra","kohlrabi"]
 
+def oxford_comma(array)
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    else
+        array[-1] = array[-1].split("").unshift("and ").join
+        #Better approach (from solution branch): array[-1].insert(0, "and ")
+        array.join(", ")
+    end
 end
